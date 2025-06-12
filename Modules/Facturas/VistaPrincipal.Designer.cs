ea163cc17c1d1cdfc99357a65ee6b01cb4b1911e
OtalTheMad <oscar454aqw@outlook.es>
2025-06-12 16:59:20 -0600

    closes #9 Punto de ventas creado closes #6 Añadido valor por defecto closes #7 Factura creada OM

---
+++ Modules/Facturas/VistaPrincipal.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ProyectoIsis.Modules.Facturas
+{
+    partial class VistaPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dgvRecibos = new System.Windows.Forms.DataGridView();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnGenerarFactura = new System.Windows.Forms.Button();
+            this.btnVerDetalle = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRecibos)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Recibos";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dgvRecibos);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 100);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 350);
+            this.panel2.TabIndex = 1;
+            // 
+            // dgvRecibos
+            // 
+            this.dgvRecibos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRecibos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvRecibos.Location = new System.Drawing.Point(0, 0);
+            this.dgvRecibos.Name = "dgvRecibos";
+            this.dgvRecibos.Size = new System.Drawing.Size(800, 350);
+            this.dgvRecibos.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btnGenerarFactura);
+            this.panel3.Controls.Add(this.btnVerDetalle);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 350);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(800, 100);
+            this.panel3.TabIndex = 2;
+            // 
+            // btnGenerarFactura
+            // 
+            this.btnGenerarFactura.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerarFactura.Location = new System.Drawing.Point(162, 32);
+            this.btnGenerarFactura.Name = "btnGenerarFactura";
+            this.btnGenerarFactura.Size = new System.Drawing.Size(115, 45);
+            this.btnGenerarFactura.TabIndex = 1;
+            this.btnGenerarFactura.Text = "Generar Factura";
+            this.btnGenerarFactura.UseVisualStyleBackColor = true;
+            // 
+            // btnVerDetalle
+            // 
+            this.btnVerDetalle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerDetalle.Location = new System.Drawing.Point(27, 32);
+            this.btnVerDetalle.Name = "btnVerDetalle";
+            this.btnVerDetalle.Size = new System.Drawing.Size(115, 45);
+            this.btnVerDetalle.TabIndex = 1;
+            this.btnVerDetalle.Text = "Ver \r\nDetalle";
+            this.btnVerDetalle.UseVisualStyleBackColor = true;
+            // 
+            // VistaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(250)))), ((int)(((byte)(240)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "VistaPrincipal";
+            this.Text = "VistaPrincipal";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRecibos)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dgvRecibos;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnVerDetalle;
+        private System.Windows.Forms.Button btnGenerarFactura;
+    }
+}
--- Modules/VistaBalance.cs
+++ Modules/VistaBalance.cs
@@ -23,8 +23,8 @@ namespace ProyectoIsis.Modules
                     return;
                 }
                 string query = @"
-                    SELECT SUM(CantidadTotal) 
-                    FROM Recibos 
+                    SELECT IDRecibo, CreadoEn, NombreCliente, Ingreso, TotalVenta, CostoEstimado, Ganancia
+                    FROM VistaBalance;
                     WHERE DATE(CreadoEn) BETWEEN @Inicio AND @Fin;";
                 using (var cmd = new SQLiteCommand(query, conn))
                 {
@@ -42,10 +42,35 @@ namespace ProyectoIsis.Modules
                 }
             }
         }
+        private void FormatoDGV()
+        {
+            try
+            {
+                dgvBalance.Columns[0].HeaderText = "ID Recibo";
+                dgvBalance.Columns[1].HeaderText = "Creado En";
+                dgvBalance.Columns[2].HeaderText = "Nombre del Cliente";
+                dgvBalance.Columns[3].HeaderText = "Ingreso";
+                dgvBalance.Columns[4].HeaderText = "Total Venta";
+                dgvBalance.Columns[5].HeaderText = "Costo Estimado";
+                dgvBalance.Columns[6].HeaderText = "Ganancia";
+                // Ajustar el ancho de las columnas
+                dgvBalance.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                dgvBalance.AllowUserToAddRows = false;
+                dgvBalance.AllowUserToDeleteRows = false;
+                dgvBalance.AllowUserToResizeColumns = true;
+                dgvBalance.ReadOnly = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al dar formato a el DataGridView: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
 
         private void btnCalcularBalance_Click(object sender, EventArgs e)
         {
             CalcularBalance();
+            FormatoDGV();
         }
+
     }
 }
